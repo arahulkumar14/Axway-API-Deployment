ea34d0bd8c8908bca95cf4e6152c54815caa5eb4
arahulkumar14 <rkumara@axway.com>
2024-08-09 14:14:42 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 echo "Starting Deployment !"
 cd ./apim_cli/apim-cli-1.14.4/scripts
 chmod 777 apim.sh
-./apim.sh apim api import -s api-env -c /apim_cli/apim-cli-1.14.4/scripts/v2/api-config.json -a /apim_cli/apim-cli-1.14.4/scripts/v2/Play\ with\ Numbers_v2.yaml.yaml -ignoreCache
+./apim.sh apim api import -s ${Env} -c /apim_cli/apim-cli-1.14.4/scripts/v2/api-config.json -a /apim_cli/apim-cli-1.14.4/scripts/v2/Play\ with\ Numbers_v2.yaml.yaml -ignoreCache
