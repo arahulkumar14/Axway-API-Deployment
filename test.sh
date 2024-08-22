114d69e96239c00662944d2eb8cc514de0ae779b
arahulkumar14 <rkumara@axway.com>
2024-08-22 15:22:10 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-echo "Starting Deployment !"
+echo "Starting Connectivity Check !"
 cd ./apim_cli/apim-cli-1.14.4/scripts
 chmod 777 apim.sh
-./apim.sh apim api import -s ${Env} -c /apim_cli/apim-cli-1.14.4/scripts/v2/api-config.json -a /apim_cli/apim-cli-1.14.4/scripts/v2/Play\ with\ Numbers_v2.yaml.yaml -ignoreCache
+./apim.sh -v
+./apim.sh org import -s ${Env} -c /apim_cli/apim-cli-1.14.4/scripts/Api_Project_File/org-config.json -ignoreCache
+./apim.sh app import -s ${Env} -c /apim_cli/apim-cli-1.14.4/scripts/Api_Project_File/application-config.json -ignoreCache
+./apim.sh apim api import -s ${Env} -c /apim_cli/apim-cli-1.14.4/scripts/Api_Project_File/api-config.json -a /apim_cli/apim-cli-1.14.4/scripts/Api_Project_File/Encryption_Decryption.json -ignoreCache
